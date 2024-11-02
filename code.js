769b492da6b59aa834c50d238bb5e5b84bb3de16
Katie Spivey <katieaspivey@gmail.com>
2024-11-02 16:43:02 -0400

    removed unneeded comments

--- code.js
+++ code.js
@@ -1,14 +1,10 @@
-// test.addEventListener("load", onLoad());
 let sec=3;
 let min;
 let hrs;
 let time;
 let timerSplit;
 let notZero=true;
-// function test()
-// {
-//     time=prompt("How long do you want to set your timer");
-// }
+
 function onLoad()
 {
     time=prompt("How long do you want to set your timer");
@@ -20,14 +16,11 @@ function onLoad()
     min=timerSplit[1];
     sec=timerSplit[2]; 
     let timeinSec=sec+(min*60)+((hrs*60)*60);
-    // let timeT=cTime+timeinSec;
-    // let timeR=timeT-cTime;
+
     alert("before")
     myInter=setInterval(function (){
         console.log("arr");
         
-        // timeR-=1; 
-        // console.log("secs:"+timeR);
         sec=Number(sec)-1;
         if(sec==0)
         {
@@ -46,8 +39,6 @@ function onLoad()
                     hrs=Number(hrs)-1;
                     min=59;
                     sec=59;
-
-
                 }
                 else
                 {
@@ -64,39 +55,8 @@ function onLoad()
     },1000)
     document.getElementById("timer").innerHTML=String(hrs).padStart(2, '0')+":"+String(min).padStart(2, '0')+":"+String(sec).padStart(2, '0');
 
-
-
-    // while(notZero)
-    // {
-    //     if(hrs==0&&min==0&&sec==0)
-    //     {
-    //         notZero=false;
-    //         document.getElementById("timer").innerHTML=hrs+":"+min+":"+sec;
-
-    //     }
-    //     // setInterval(updateTimer, 1000);
-    //     // setTimeout(minSec, 1000);
-    //     // setInterval(minMin, 60000);
-    //     // setInterval(minHour, 3600000);
-    //     // setInterval(function()
-    //     // {
-    //     //     let cTime=Date.now();
-
-    //     // })
-
-
-        
-        
-
-
-    // }
-    // // alert("gi");
-
-   
-
-
-
 }
+
 function minSec()
 {
     sec=sec-1
@@ -106,8 +66,6 @@ function minSec()
         sec=59;
     }
     document.getElementById("timer").innerHTML=hrs+":"+min+":"+sec;
-
-
 }
 function minMin()
 {
@@ -117,27 +75,14 @@ function minMin()
     {
         min=59;
     }
-    document.getElementById("timer").innerHTML=hrs+":"+min+":"+sec;
-
-    
-    
-
+    document.getElementById("timer").innerHTML=hrs+":"+min+":"+sec; 
 }
+
 function minHour()
 {
     hrs=hrs-1
-
-    // if(hrs>0&&min==0)
-    // {
-    //     min=59;
-    // }
     document.getElementById("timer").innerHTML=hrs+":"+min+":"+sec;
-
-    
-
 }
-
-
 
 function updateTimer()
 {
@@ -164,6 +109,4 @@ function updateTimer()
     {
         notZero=false;
     }
-
-
 }
