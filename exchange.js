7437367ba38b2f04484c361b8ba51a8f875a02e8
thehandyman28 <51925740+thehandyman28@users.noreply.github.com>
2024-11-03 09:57:31 -0500

    update

--- exchange.js
+++ exchange.js
@@ -1,11 +1,13 @@
 let credit=4;
-localStorage.setItem("onwed", 1,2,3);
+// localStorage.setItem("onwed", 1,2,3);
 localStorage.setItem("useing", 1);
 let buyMsg="buy";
 let owndMsg="use"
 let useing="useing";
 function onwing()
 {
+    localStorage.setItem("onwed",localStorage.getItem("onwed") || '1,2,3');
+
     let onwedList=localStorage.getItem("onwed");
     console.log("re"+onwedList);
     let i=0;
@@ -34,11 +36,51 @@ function onwing()
         
         
         let a =Math.floor(onwedList%10)
+        console.log("num:"+Number(ownListTwo[i]));
         let idTest="I"+Number(ownListTwo[i]);
         console.log("idName "+idTest)
         console.log("elemtns:"+document.getElementById(idTest));
         document.getElementById(idTest).innerHTML="use";
         onwedList=Math.floor(onwedList/10);
+    }
+    let dis=localStorage.getItem("useing");
+    
+    let idIn="I"+Number(dis);
+
+    document.getElementById(idIn).innerHTML="useing";
+    document.getElementById(idIn).disabled = true;
+
+
+
+}
+function seeIfBuy(money,inUse,cost,id,num)
+{
+        if(money<cost)
+        {
+            document.getElementById(id).disabled=true;
+            document.getElementById(id).innerHTML="Buy"+[cost];
+
+        }
+        else
+        {
+            if(inUse!=num)
+            {
+
+            }
+        }
+}
+function btn2()
+{
+    if(credit<2)
+    {
+        document.getElementById(btn2).disabled=true;
+    }
+    else
+    {
+        if(useing!=2)
+        {
+
+        }
     }
 
 }
