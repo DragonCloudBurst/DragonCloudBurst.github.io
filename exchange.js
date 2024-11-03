6be8ede289750acf0b78fdf5975c8311ebdbc3d0
thehandyman28 <51925740+thehandyman28@users.noreply.github.com>
2024-11-02 20:28:04 -0400

    add more local vars

--- exchange.js
+++ exchange.js
@@ -1,8 +1,17 @@
 localStorage.setItem("credits", 3);
+localStorage.setItem("onwed", 123);
+localStorage.used("usssing", 1);
+
+
 
 function eLoad()
 {
     alert("gi");
     document.getElementById("credit-count").innerHTML="You have "+localStorage.getItem("credits")+" credits.";
 
+}
+function onwing()
+{
+    let onwedList=localStorage.getItem("onwed");
+    while()
 }
